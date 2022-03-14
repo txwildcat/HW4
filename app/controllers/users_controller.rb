766e3e393d9e420e2bd6a6455a51edb3d1d74705
txwildcat <tyler.askren@kellogg.northwestern.edu>
2022-03-14 02:39:27 +0000

    added bcrypt to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params["user"])
+    @user.password = BCrypt::Password.create(params["user"]["password"])
     @user.save
     redirect_to "/users/#{@user.id}"
   end
