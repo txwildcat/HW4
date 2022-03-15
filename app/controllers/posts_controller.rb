760097346584d66176bdb9f9de94d7e380d11f59
txwildcat <tyler.askren@kellogg.northwestern.edu>
2022-03-15 06:56:06 +0000

    might be stuck now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,22 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
     @post.place_id = params["place_id"]
   end
 
   def create
-    @post = Post.new(params["post"])
-    @post.user_id = @current_user.id
-    @post.save
+    if @current_user
+      @post = Post.new(params["post"])
+      @post.user_id = @current_user.id
+      @post.save
+    else
+      flash[:notice] = "Please Login"
+    end
     redirect_to "/places/#{@post.place.id}"
   end
 
